ece1da6434b4a60ea644cbbef1d8ebf4475ff018
Taeho KIM <126849653+taeho4523@users.noreply.github.com>
2024-08-24 19:31:55 +0900

    Revert "2024-굿나잇-해커톤"

--- src/main/java/com/wishes/techeertree/entity/WishStatus.java
+++
@@ -1,7 +0,0 @@
-package com.wishes.techeertree.entity;
-
-public enum WishStatus {
-    APPROVED,   // 승인됨
-    PENDING,    // 보류됨
-    REJECTED    // 거절됨
-}
--- src/main/java/com/wishes/techeertree/repository/WishRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wishes.techeertree.repository;
-
-import com.wishes.techeertree.entity.Wish;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WishRepository extends JpaRepository<Wish, Long> {
-}
