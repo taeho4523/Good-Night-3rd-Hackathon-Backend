da4618513446213279dfefc37cbbbe8c1fe99e9d
Taeho KIM <laferrari963@naver.com>
2024-08-24 17:53:25 +0900

    wishregister

---
+++ src/main/java/com/wishes/techeertree/TecheerTreeApplication.java
@@ -0,0 +1,13 @@
+package com.wishes.techeertree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecheerTreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TecheerTreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wishes/techeertree/TecheerTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wishes.techeertree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecheerTreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
